18290fe87de3afcdb493596c4d9bb59d158a51f3
jituthecoder <jituthecoder@gmail.com>
2024-01-02 10:37:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import {generate} from 'critical';
+import express from 'express'
+import bodyParser from 'body-parser';
+const app = express();
+var port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+  var url =req.body.url;
+  var key = req.body.key;
+  var _wpnonce = req.body._wpnonce;
+  var filename = req.body.filename;
+  var css_url = req.body.css_url;
+  var path = req.body.path;
+  var html = req.body.html;
+  if (url== null) {
+    url = 'https://w3developments.com';
+  }
+    const cssGenerate = async() =>{
+      res.setHeader('Content-Type', 'application/json');
+      const {css, html, uncritical} = await generate({
+        src: url,
+        width: 1300,
+        height: 900,
+      });
+
+      const data = {
+        url: url,
+        w3_put_preload_css: 1,
+        result:'success',
+        _wpnonce: _wpnonce,
+        filename:filename,
+        path:path,
+        w3_css:css
+      };      
+      res.send(JSON.stringify(data));
+    }
+    cssGenerate();
+    
+});
+app.listen(port, () => {
+    console.log(`Critical Css app listening on port ${port}`)
+  })
